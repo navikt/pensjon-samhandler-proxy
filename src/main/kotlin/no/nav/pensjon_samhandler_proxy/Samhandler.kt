64c517e0b07814f9a902ba9cb1a8023a57eef849
Morten Normann <morten.normann@nav.no>
2024-08-13 10:24:13 +0200

    Map TOutputElementer til Samhandler

---
+++ src/main/kotlin/no/nav/pensjon_samhandler_proxy/Samhandler.kt
@@ -0,0 +1,47 @@
+package no.nav.pensjon_samhandler_proxy
+
+data class Samhandler(
+    val navn: String?,
+    val sprak: String? = null,
+    val samhandlerType: String?,
+    val offentligId: String?,
+    val idType: String?,
+    val avdelinger: List<Avdeling>?,
+)
+
+data class Avdeling(
+    val idTSSEkstern: String?,
+    val avdelingNavn: String?,
+    val avdelingType: String? = null,
+    val avdelingsnr: String?,
+    val ePost: String? = null,
+    val telefon: String? = null,
+    val mobil: String? = null,
+    val kontoer: List<Konto>? = null,
+    val aAdresse: Adresse? = null,
+    val pAdresse: Adresse? = null,
+    val tAdresse: Adresse? = null,
+    val uAdresse: Adresse? = null,
+)
+
+data class Konto(
+    val kontoType: String?,
+    val kontonummer: String?,
+    val banknavn: String?,
+    val bankkode: String?,
+    val swiftkode: String?,
+    val valuta: String?,
+    val bankadresse: Adresse?,
+)
+
+data class Adresse(
+    val adresselinje1: String?,
+    val adresselinje2: String?,
+    val adresselinje3: String?,
+    val adresseTypekode: String? = null,
+    val postNr: String? = null,
+    val poststed: String? = null,
+    val land: String?,
+    val kommuneNr: String? = null,
+    val erGyldig: Boolean? = null,
+)
--- src/main/kotlin/no/nav/pensjon_samhandler_proxy/SamhandlerViaKoe.kt
+++ src/main/kotlin/no/nav/pensjon_samhandler_proxy/SamhandlerViaKoe.kt
@@ -16,7 +16,15 @@ import javax.xml.transform.stream.StreamSource
 class SamhandlerViaKoe(
     private val xmlJmsTemplate: JmsTemplate,
 ) {
-    fun hentSamhandlerXml(tssId: String, detaljert: Boolean): TOutputElementer? {
+    fun hentSamhandlerXml(tssId: String, detaljert: Boolean): Samhandler? {
+        return if (detaljert) {
+            hentSamhandlerXmlRaw(tssId, detaljert)?.samhandlerODataB910?.enkeltSamhandler?.first()?.toSamhandler()
+        } else {
+            hentSamhandlerXmlRaw(tssId, detaljert)?.samhandlerODataB980?.ident?.first()?.toSamhandler()
+        }
+    }
+
+    fun hentSamhandlerXmlRaw(tssId: String, detaljert: Boolean): TOutputElementer? {
         return if (detaljert) {
             kallSamhandlerXml(TssSamhandlerData().apply {
                 tssInputData = TssSamhandlerData.TssInputData().apply {
@@ -43,7 +51,20 @@ class SamhandlerViaKoe(
         }
     }
 
-    fun finnSamhandlerXml(navn: String?, idType: String?, offentligId: String?, samhandlerType: String?): TOutputElementer? {
+    fun finnSamhandlerXml(navn: String?, idType: String?, offentligId: String?, samhandlerType: String?): List<Samhandler> {
+        return if ((navn?.isNotBlank() == true) && idType.isNullOrBlank() && offentligId.isNullOrBlank()) {
+            finnSamhandlerXmlRaw(navn, idType, offentligId, samhandlerType)?.samhandlerODataB98A?.ident?.map { it.toSamhandler() } ?: emptyList()
+        } else if (((idType?.isNotBlank() == true) && (offentligId?.isNotBlank() == true)) && navn.isNullOrBlank()) {
+            finnSamhandlerXmlRaw(navn, idType, offentligId, samhandlerType)?.samhandlerODataB910?.enkeltSamhandler?.map { it.toSamhandler() } ?: emptyList()
+        } else if ((idType.isNullOrBlank() && offentligId.isNullOrBlank()) && navn.isNullOrBlank() && (samhandlerType?.isNotBlank() == true)) {
+            finnSamhandlerXmlRaw(navn, idType, offentligId, samhandlerType)?.samhandlerODataB940?.enkeltSamhandler?.map { it.toSamhandler() } ?: emptyList()
+        } else {
+            //Ugyldig input. Kast exception
+            throw RuntimeException("Ugyldig input")
+        }
+    }
+
+    fun finnSamhandlerXmlRaw(navn: String?, idType: String?, offentligId: String?, samhandlerType: String?): TOutputElementer? {
         return if ((navn?.isNotBlank() == true) && idType.isNullOrBlank() && offentligId.isNullOrBlank()) {
             kallSamhandlerXml(TssSamhandlerData().apply {
                 tssInputData = TssSamhandlerData.TssInputData().apply {
@@ -126,5 +147,104 @@ class SamhandlerViaKoe(
             val unmarshal = unmarshaller.unmarshal(xmlInputFactory.createXMLStreamReader(StreamSource(StringReader(text.replace("samhandlerODataB982", "samhandlerODataB98A")))), TssSamhandlerData::class.java)
             return unmarshal.value.tssOutputData
         }
+    }
+
+    fun no.nav.freg.tss.Samhandler.toSamhandler(): Samhandler {
+        return Samhandler(
+            navn = samhandler110?.samhandler?.first()?.navnSamh,
+            sprak = samhandler110?.samhandler?.first()?.kodeSpraak,
+            samhandlerType = samhandler110?.samhandler?.first()?.kodeSamhType,
+            offentligId = samhandler110?.samhandler?.first()?.idOff,
+            idType = samhandler110?.samhandler?.first()?.kodeIdentType,
+            avdelinger = samhandlerAvd125?.samhAvd?.map { mapToAvdeling(it, this.adresse130, this.konto140, this.kontakter150) },
+        )
+    }
+
+    fun IdentOffEks.toSamhandler(): Samhandler {
+        return Samhandler(
+            offentligId = idOff,
+            idType = kodeIdentType,
+            samhandlerType = kodeSamhType,
+            navn = navnSamh,
+            avdelinger = listOf(Avdeling(
+                avdelingNavn = avdelingsNavn?.trim(),
+                avdelingsnr = avdelingsNr,
+                idTSSEkstern = idOffTSS,
+                )),
+        )
+    }
+
+    fun IdentOffEks2.toSamhandler(): Samhandler {
+        return Samhandler(
+            offentligId = idOff,
+            idType = kodeIdentType,
+            samhandlerType = kodeSamhType,
+            navn = navnSamh,
+            avdelinger = listOf(Avdeling(
+                avdelingNavn = avdelingsNavn?.trim(),
+                avdelingType = avdelingsType?.trim(),
+                avdelingsnr = avdelingsNr,
+                idTSSEkstern = idOffTSS,
+            ))
+        )
+    }
+
+    fun TypeKomp940.toSamhandler(): Samhandler {
+        return Samhandler(
+            navn = samhandler110?.samhandler?.first()?.navnSamh,
+            sprak = samhandler110?.samhandler?.first()?.kodeSpraak,
+            samhandlerType = samhandler110?.samhandler?.first()?.kodeSamhType,
+            offentligId = samhandler110?.samhandler?.first()?.idOff,
+            idType = samhandler110?.samhandler?.first()?.kodeIdentType,
+            avdelinger = samhandlerAvd125?.samhAvd?.map { mapToAvdeling(it, this.adresse130, null, null) },
+        )
+    }
+
+    private fun mapToAvdeling(avdelingDto: SamhAvdPraType, adresse130: TypeSamhAdr?, konto140: TypeSamhKonto?, kontakter150: TypeSamhKontakt?): Avdeling {
+        return Avdeling(
+            idTSSEkstern = avdelingDto.idOffTSS,
+            avdelingNavn = avdelingDto.avdNavn?.trim(),
+            avdelingType = avdelingDto.typeAvd?.trim(),
+            avdelingsnr = avdelingDto.avdNr,
+            pAdresse = adresse130?.adresseSamh?.firstOrNull { it.avdNr == avdelingDto.avdNr && it.kodeAdresseType == "PST" }?.asAdresse(),
+            aAdresse = adresse130?.adresseSamh?.firstOrNull { it.avdNr == avdelingDto.avdNr && it.kodeAdresseType == "WP" }?.asAdresse(),
+            tAdresse = adresse130?.adresseSamh?.firstOrNull { it.avdNr == avdelingDto.avdNr && it.kodeAdresseType == "TILL" }?.asAdresse(),
+            uAdresse = adresse130?.adresseSamh?.firstOrNull { it.avdNr == avdelingDto.avdNr && it.kodeAdresseType == "UTL" }?.asAdresse(),
+            kontoer = konto140?.konto?.filter { it.avdNr == avdelingDto.avdNr }?.map { it.asKonto() },
+            ePost = kontakter150?.enKontakt?.firstOrNull { it.avdNr == avdelingDto.avdNr && it.kodeKontaktType == "EPOS" }?.kontakt,
+            telefon = kontakter150?.enKontakt?.firstOrNull { it.avdNr == avdelingDto.avdNr && it.kodeKontaktType == "TLF" }?.kontakt,
+            mobil = kontakter150?.enKontakt?.firstOrNull { it.avdNr == avdelingDto.avdNr && it.kodeKontaktType == "MTLF" }?.kontakt,
+        )
+    }
+
+    private fun KontoType.asKonto(): Konto {
+        return Konto(
+            bankadresse = Adresse(
+                adresselinje1 = this.utlBankAdrInfo?.utlBankAdr?.get(0)?.adresse?.get(0)?.trim(),
+                adresselinje2 = this.utlBankAdrInfo?.utlBankAdr?.get(0)?.adresse?.get(1)?.trim(),
+                adresselinje3 = this.utlBankAdrInfo?.utlBankAdr?.get(0)?.adresse?.get(2)?.trim(),
+                land = this.kodeLand?.trim(),
+            ),
+            bankkode = this.bankKode?.trim(),
+            banknavn = this.bankNavn?.trim(),
+            kontonummer = (this.gironrInnland ?: this.gironrUtland)?.trim(),
+            kontoType = this.kodeKontoType?.value(),
+            swiftkode = this.swiftKode?.trim(),
+            valuta = this.kodeValuta?.trim(),
+        )
+    }
+
+    private fun AdresseSamhType.asAdresse(): Adresse {
+        return Adresse(
+            adresselinje1 = this.adrLinjeInfo?.adresseLinje?.getOrNull(0),
+            adresselinje2 = this.adrLinjeInfo?.adresseLinje?.getOrNull(1),
+            adresselinje3 = this.adrLinjeInfo?.adresseLinje?.getOrNull(2),
+            adresseTypekode = this.kodeAdresseType,
+            postNr = this.postNr?.trim(),
+            poststed = this.poststed?.trim(),
+            land = this.kodeLand,
+            kommuneNr = this.kommuneNr?.trim(),
+            erGyldig = this.gyldigAdresse == "J",
+        )
     }
 }
